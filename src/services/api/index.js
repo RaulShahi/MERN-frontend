0305b485003ee8b08de2352878bd29d691db2362
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-03-27 18:00:27 +0545

    api export

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -8,3 +8,14 @@ const BASE_API_PATH_LOGIN = `${BASE_API_PATH_USERS}/login`;
 //Places
 const BASE_API_PATH_PLACES = `${BASE_API_PATH}places`;
 const BASE_API_PATH_USER_PLACES = `${BASE_API_PATH_PLACES}/user`;
+
+export const Apis = {
+  //Users
+  Users: BASE_API_PATH_USERS,
+  Signup: BASE_API_PATH_SIGN_UP,
+  Login: BASE_API_PATH_LOGIN,
+
+  //Places
+  Places: BASE_API_PATH_PLACES,
+  UserPlaces: BASE_API_PATH_USER_PLACES,
+};
